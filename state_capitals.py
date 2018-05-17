b2e2095b2e264514feb9b18dad144732dc871063
Jason Spiller <jason@thetargetlab.com>
2018-05-16 19:16:21 -0600

    Game is complete.

--- state_capitals.py
+++ state_capitals.py
@@ -1,4 +1,5 @@
 """State Captials Game."""
+from random import shuffle
 
 state_dict = {
     "Alabama": "Montgomery",
@@ -15,13 +16,37 @@ class StateCapitalsGame():
 
     def __init__(self, dictionary):
         """So you say you need a comment."""
-        self.dictionary = dictionary
+        self.dict_states = {}
+        self.stats = {'Correct': 0, 'Incorrect': 0}
+        keys = list(dictionary.keys())
+        shuffle(keys)
+        for key in keys:
+            self.dict_states.update({key: dictionary[key]})
+        print('Welcome to the State Capitals Game.')
+        print('Below you will be asked the capital of a state.')
+        print('Please type in the capital and enter.')
+        print('Continue doing so until your see your stats.')
+        for key, value in self.dict_states.items():
+            question = ('What is the capital of {}? ').format(key)
+            answer = input(question)
+            if(answer == value):
+                print('Yay, you are CORRECT!')
+                self.stats['Correct'] += 1
+                print(("You currenty have {} right and {} wrong.").format(self.stats['Correct'], self.stats['Incorrect']))
+            else:
+                print('Sorry, you are incorrect.')
+                self.stats['Incorrect'] += 1
+                print(("You currenty have {} right and {} wrong.").format(self.stats['Correct'], self.stats['Incorrect']))
+        play_again = input("Would you like to play again? Type y for yes and n for no. ")
+        if (play_again == 'y'):
+            StateCapitalsGame(state_dict)
+        else:
+            print(("Thanks for playing. You got {} right and {} wrong.").format(self.stats['Correct'], self.stats['Incorrect']))
+
 
     def __str__(self):
         """So you say you need a comment."""
-        return ("Our dict is {} long.").format(len(self.dictionary))
+        return ("You got {} right and {} wrong.").format(self.stats['Correct'], self.stats['Incorrect'])
 
 
 game1 = StateCapitalsGame(state_dict)
-
-print(game1)
