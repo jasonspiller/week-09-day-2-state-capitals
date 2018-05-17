2a0a742cb32738b39638c9597da33fef2f82390d
Jason Spiller <jason@thetargetlab.com>
2018-05-16 19:32:06 -0600

    Broke out everything into methods.

--- state_capitals.py
+++ state_capitals.py
@@ -18,25 +18,45 @@ class StateCapitalsGame():
         """So you say you need a comment."""
         self.dict_states = {}
         self.stats = {'Correct': 0, 'Incorrect': 0}
-        keys = list(dictionary.keys())
-        shuffle(keys)
-        for key in keys:
-            self.dict_states.update({key: dictionary[key]})
+        self.shuffle_states(dictionary)
+        self.instructions()
+        self.play_game()
+        self.play_again()
+
+    def instructions(self):
+        """So you say you need a comment."""
         print('Welcome to the State Capitals Game.')
         print('Below you will be asked the capital of a state.')
         print('Please type in the capital and enter.')
-        print('Continue doing so until your see your stats.')
+        
+
+    def shuffle_states(self, state_dictionary):
+        """So you say you need a comment."""
+        keys = list(state_dictionary.keys())
+        shuffle(keys)
+        for key in keys:
+            self.dict_states.update({key: state_dictionary[key]})
+
+    def play_game(self):
+        """So you say you need a comment."""
         for key, value in self.dict_states.items():
             question = ('What is the capital of {}? ').format(key)
             answer = input(question)
-            if(answer == value):
-                print('Yay, you are CORRECT!')
-                self.stats['Correct'] += 1
-                print(("You currenty have {} right and {} wrong.").format(self.stats['Correct'], self.stats['Incorrect']))
-            else:
-                print('Sorry, you are incorrect.')
-                self.stats['Incorrect'] += 1
-                print(("You currenty have {} right and {} wrong.").format(self.stats['Correct'], self.stats['Incorrect']))
+            self.process_answer(answer, value)
+
+    def process_answer(self, input, capital):
+        """So you say you need a comment."""
+        if(input == capital):
+            print('Yay, you are CORRECT!')
+            self.stats['Correct'] += 1
+            print(("You currenty have {} right and {} wrong.").format(self.stats['Correct'], self.stats['Incorrect']))
+        else:
+            print('Sorry, you are incorrect.')
+            self.stats['Incorrect'] += 1
+            print(("You currenty have {} right and {} wrong.").format(self.stats['Correct'], self.stats['Incorrect']))
+
+    def play_again(self):
+        """So you say you need a comment."""
         play_again = input("Would you like to play again? Type y for yes and n for no. ")
         if (play_again == 'y'):
             StateCapitalsGame(state_dict)
